924e7cbd9f4cd592098fb0b904eef50323c3b789
seoco <seokho.dev@gmail.com>
2018-05-20 16:47:42 +0900

    개별 유저에 대한 상품 추천 기능(학습, 예측, 결과 조회) 구현 완료

--- controllers/RecommendControllers.js
+++ controllers/RecommendControllers.js
@@ -1,11 +1,38 @@
+const axios = require('axios');
+const mongoose = require('mongoose');
 const Review = require('../models/Review');
 const User = require('../models/User');
 const Product = require('../models/Product');
 
+const url = 'http://localhost:8000';
+
 module.exports = {
-  trainOneUserRequest(req, res) {
-    // 1. 학습용 데이터 요청 쿼리 및 가공
-    // 2. IC 서버에 데이터 전달 및 학습 요청
+  async trainOneUserRequest(req, res) {
+    console.log('requesting train for one user...');
+
+    const { id } = req.params;
+    const fetched = await fetchTrainData(id);
+    const payload = { id, data: processTrainData(fetched) };
+    const dataRes = await axios.post(`${url}/train_data/`, payload);
+    console.log(dataRes.data);
+    const trainRes = await axios.post(`${url}/train/`, id);
+    console.log(trainRes.data);
+
+    res.send('training is done');
+  },
+
+  async predictOneUserRequest(req, res) {
+    console.log('requesting predict for one user...');
+
+    const { id } = req.params;
+    const fetched = await fetchPredictData(id);
+    const payload = { id, data: fetched };
+    const dataRes = await axios.post(`${url}/predict_data/`, payload);
+    console.log(dataRes.data);
+    const predictRes = await axios.post(`${url}/predict/`, id);
+    console.log(predictRes.data);
+
+    res.send('prediction is done');
   },
 
   trainAllUserRequest(req, res) {
@@ -13,26 +40,81 @@ module.exports = {
     // 2. trainOneUserRequest를 반복 수행
   },
 
-  predictOneUserRequest(req, res) {
-    // 1. 예측용 데이터 요청 쿼리 및 가공
-    // 2. IC 서버에 데이터 전달 및 예측 요청
-  },
-
   predictAllUserRequest(req, res) {
     // 1. 전체 유저 목록 쿼리
     // 2. predictOneUserRequest를 반복 수행
   },
 
-  fetchRecommendItems(req, res) {
-    // 1. 유저별 저장된 학습 결과 조회
-    // 2. 클라이언트에 출력하기 위해 가공 및 전달
+  async fetchRecommendItems(req, res) {
+    const { id } = req.params;
+    const result = await axios.post(`${url}/predict_result/`, id);
+
+    res.send(result.data);
   }
 };
 
-const train = () => {
-  //
-};
+const fetchTrainData = userId =>
+  new Promise(resolve => {
+    Review.find({ userId })
+      .populate({ path: 'productId', select: { _id: 1, avgTaste: 1 } })
+      .select({
+        _id: 0,
+        productId: 1,
+        score: 1
+      })
+      .exec((err, doc) => {
+        if (!err) {
+          resolve(doc);
+          console.log('train data has fetched');
+        }
+      });
+  });
 
-const predict = () => {
-  //
+const fetchPredictData = userId =>
+  new Promise(resolve => {
+    Product.aggregate([
+      {
+        $lookup: {
+          from: Review.collection.name,
+          localField: 'reviews',
+          foreignField: '_id',
+          as: 'reviews'
+        }
+      },
+      { $unwind: { path: '$reviews', preserveNullAndEmptyArrays: true } },
+      {
+        $group: {
+          _id: '$_id',
+          avgTaste: { $first: '$avgTaste' },
+          reviews: { $push: '$reviews' }
+        }
+      },
+      {
+        $match: {
+          reviews: {
+            $not: { $elemMatch: { userId: mongoose.Types.ObjectId(userId) } }
+          }
+        }
+      },
+      { $project: { _id: 0, productId: '$_id', avgTaste: 1 } },
+      { $sort: { productId: 1 } }
+    ]).exec((err, doc) => {
+      if (!err) {
+        resolve(doc);
+        console.log('predict data has fetched');
+      }
+    });
+  });
+
+const processTrainData = data => {
+  const processed = data.map(item => {
+    return {
+      productId: item.productId._id,
+      avgTaste: item.productId.avgTaste,
+      score: item.score
+    };
+  });
+
+  console.log('train data has processed');
+  return processed;
 };
