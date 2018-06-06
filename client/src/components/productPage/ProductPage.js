1d5ac61c3d3c137f5774951b85d2eefc8767b275
seoco <seokho.dev@gmail.com>
2018-06-06 20:43:17 +0900

    맛수치 출력 구현

--- client/src/components/productPage/ProductPage.js
+++ client/src/components/productPage/ProductPage.js
@@ -3,40 +3,63 @@ import axios from 'axios';
 import WishButton from './WishButton';
 import MyReview from './MyReview';
 import ProductReviews from './ProductReviews';
+import category from '../../productCategoryDict';
 
 class ProductPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: {}
+      product: null
     };
 
     this.productId = props.match.params.id;
   }
 
   componentDidMount() {
-    // const { id } = this.props.match.params;
     axios.get(`/api/product/${this.productId}`).then(res => {
       this.setState({ product: res.data });
     });
   }
 
-  render() {
+  renderBasicInfos() {
+    const { product } = this.state;
+
     return (
       <div>
         <h3>상품 조회 페이지</h3>
         <div>
           <ul>
-            <li>{this.state.product.name}</li>
+            <li>{product.name}</li>
+            <li>{product.category}</li>
             <WishButton productId={this.productId} />
             <hr />
             <h4>상품 정보</h4>
-            <li>{this.state.product.details}</li>
+            <li>{product.details}</li>
             <hr />
             <MyReview productId={this.productId} />
             <ProductReviews productId={this.productId} />
           </ul>
         </div>
+      </div>
+    );
+  }
+
+  renderTastes() {
+    const { product } = this.state;
+    const taste = category[product.category].params;
+
+    return taste.map((item, i) => {
+      return <li key={i}>{item}: {product.avgTaste[i]}</li>;
+    });
+  }
+
+  render() {
+    const { product } = this.state;
+
+    return (
+      <div>
+        {product && this.renderBasicInfos()}
+        <ul>{product && this.renderTastes()}</ul>
       </div>
     );
   }
