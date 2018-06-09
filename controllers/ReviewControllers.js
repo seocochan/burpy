7019243b8f23c42672bf199d081cc36f191180b7
LeeYoonSang <kty189@naver.com>
2018-06-09 21:37:16 +0900

    임시저장

--- controllers/ReviewControllers.js
+++ controllers/ReviewControllers.js
@@ -21,23 +21,11 @@ module.exports = {
     const values = { userId, ...req.body };
 
     const newReview = await new Review(values).save();
-    const Avg = await Review.aggregate([
-      { $match : { productId : newReview.productId} },
-      { $group : {
-        _id : "",
-        scoreavg : {$avg : "$score"}
-      }}
-    ])
-    console.log(Avg[0].scoreavg)
-    const productScore = await Product.findByIdAndUpdate(
-      newReview.productId,
-      {
-        avgScore : Avg[0].scoreavg
-      }
-      ,{new : true}
-    )
+    console.log(newReview.productId)
+    const Avg = fetchScore(newReview.productId)
     console.log(Avg)
-    res.send(productScore);
+    res.send(Avg)
+    //const productScore = await fetchProduct(newReview.productId,Avg[0].scoreavg)
   },
 
   async updateReview(req, res) {
@@ -61,10 +49,10 @@ module.exports = {
     res.send(productScore);
   },
 
-  async removeReview(req, res) {
+  removeReview(req, res) {
     const { id } = req.params;
 
-    const removereview = await Review.findOne({ _id: id }, (err, doc) => {
+    Review.findOne({ _id: id },async (err, doc) => {
       doc.remove(err => {
         if (err) {
           console.warn(err);
@@ -72,9 +60,31 @@ module.exports = {
         }
         //res.status(200).send(id);
       });
+      const Avg = await Review.aggregate([
+        { $match : { 
+          $and : [
+            {productId : doc.productId},
+            {_id : {$ne : id}}
+          ]}}
+          ,
+        { $group : {
+          _id : "",
+          scoreavg : {$avg : "$score"}
+        }}
+      ])
+      console.log(Avg)
+      const productScore = await Product.findByIdAndUpdate(
+        doc.productId,
+        {
+          avgScore : Avg[0].scoreavg
+        }
+        ,{new : true}
+      )
+      console.log(productScore);
+      res.send(productScore);
     });
     console.log(id)
-    console.log(removereview.productId);
+    /*console.log(removereview.productId);
     const Avg = await Review.aggregate([
       { $match : { 
         $and : [
@@ -96,7 +106,7 @@ module.exports = {
       ,{new : true}
     )
     console.log(productScore);
-    res.send(productScore);
+    res.send(productScore);*/
 
   },
 
@@ -137,3 +147,38 @@ module.exports = {
       });
   }
 };
+
+const fetchScore = Id =>{
+  new Promise(resolve => {
+    Review.aggregate([
+      { $match : { productId : Id} },
+      { $group : {
+        _id : "",
+        scoreavg : {$avg : "$score"}
+      }}
+    ])
+    .exec((err,doc)=>{
+      if(!err){
+        console.log(doc);
+        resolve(doc);
+      }
+    })
+  })
+}
+
+const fetchProduct = (Id,score) =>{
+  new Promise(resolve =>{
+    Product.findByIdAndUpdate(
+      Id,
+      { avgScore : score},
+      {new : true}
+    )
+    .exec((err,doc)=>{
+      if(!err){
+        resolve(doc);
+        console.log(doc);
+      }
+    })
+  })
+}
+
