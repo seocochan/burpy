9a631b97a397654dff005134bb2d36b0e901bd20
seoco <seokho.dev@gmail.com>
2018-07-15 03:54:25 +0900

    반응형 드로어 구현

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,32 +4,35 @@ import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import { withStyles } from '@material-ui/core/styles';
 import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
   AppBar,
   Toolbar,
   Typography,
   IconButton,
   MenuItem,
-  Menu
+  Menu,
+  Button,
+  Hidden
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import { AccountCircle } from '@material-ui/icons';
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  flex: {
-    flex: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-});
+import { Home, ThumbUp, LocalBar, AccountCircle } from '@material-ui/icons';
 
 class Header extends Component {
   state = {
-    anchorEl: null
+    drawer: false,
+    anchorEl: null,
+    value: 0
+  };
+
+  toggleDrawer = open => () => {
+    this.setState({
+      drawer: open
+    });
   };
 
   handleMenu = event => {
@@ -40,93 +43,144 @@ class Header extends Component {
     this.setState({ anchorEl: null });
   };
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
     const { classes, auth } = this.props;
-    const { anchorEl } = this.state;
+    const { drawer, anchorEl, value } = this.state;
     const open = Boolean(anchorEl);
     console.log(open);
     return (
       <div className={classes.root}>
+        <Drawer open={drawer} onClose={this.toggleDrawer(false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer(false)}
+            onKeyDown={this.toggleDrawer(false)}
+          >
+            <List>
+              <div className={classes.drawerList}>
+                <ListItem button component={Link} to="/recommend">
+                  <ListItemIcon>
+                    <ThumbUp className={classes.icon} />
+                  </ListItemIcon>
+                  추천음료
+                </ListItem>
+                <ListItem button component={Link} to="/my-products">
+                  <ListItemIcon>
+                    <LocalBar className={classes.icon} />
+                  </ListItemIcon>
+                  마이바
+                </ListItem>
+                <Divider />
+              </div>
+            </List>
+          </div>
+        </Drawer>
         <AppBar position="fixed">
           <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-              component={Link}
-              to="/"
-            >
-              <MenuIcon />
-            </IconButton>
+            <Hidden smUp implementation="css">
+              <IconButton
+                className={classes.drawerButton}
+                color="inherit"
+                aria-label="Menu"
+                onClick={this.toggleDrawer(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
             <Typography
               variant="title"
               color="inherit"
-              className={classes.flex}
+              className={classes.title}
+              component={Link}
+              to="/"
             >
               BURPY
             </Typography>
+            <Hidden xsDown implementation="css">
+              <Button component={Link} to="/recommend">
+                <ThumbUp className={classes.icon} />
+                추천음료
+              </Button>
+              <Button component={Link} to="/my-products">
+                <LocalBar className={classes.icon} />
+                마이바
+              </Button>
+            </Hidden>
             <SearchBar />
-            {auth && (
-              <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : null}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
-                >
-                  <MenuItem
-                    onClick={this.handleClose}
-                    component={Link}
-                    to="/recommend"
+            <Hidden xsDown implementation="css">
+              {auth && (
+                <div>
+                  <IconButton
+                    aria-owns={open ? 'menu-appbar' : null}
+                    aria-haspopup="true"
+                    onClick={this.handleMenu}
+                    color="inherit"
                   >
-                    추천 상품
-                  </MenuItem>
-                  <MenuItem
-                    onClick={this.handleClose}
-                    component={Link}
-                    to="/wishlist"
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    open={open}
+                    onClose={this.handleClose}
                   >
-                    찜 목록
-                  </MenuItem>
-                  <MenuItem
-                    onClick={this.handleClose}
-                    component={Link}
-                    to="/my-products"
-                  >
-                    내 상품
-                  </MenuItem>
-                  <MenuItem
-                    onClick={this.handleClose}
-                    component="a"
-                    href="/api/logout"
-                  >
-                    로그아웃
-                  </MenuItem>
-                </Menu>
-              </div>
-            )}
+                    <MenuItem
+                      onClick={this.handleClose}
+                      component={Link}
+                      to="/wishlist"
+                    >
+                      찜 목록
+                    </MenuItem>
+                    <MenuItem
+                      onClick={this.handleClose}
+                      component="a"
+                      href="/api/logout"
+                    >
+                      로그아웃
+                    </MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Hidden>
           </Toolbar>
         </AppBar>
       </div>
     );
   }
 }
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flex: 1
+  },
+  drawerButton: {
+    marginLeft: '-1rem',
+    marginRight: '1rem'
+  },
+  drawerList: {
+    width: 250
+  },
+  icon: {
+    marginRight: theme.spacing.unit,
+    fontSize: 20
+  }
+});
 
 function mapStateToProps({ auth }) {
   return { auth };
