7d22d99d253b42fdaa0291c26f2890ba00e4be1a
seoco <seokho.dev@gmail.com>
2018-07-21 03:08:04 +0900

    상품 등록/편집 텍스트 에디터 탑재

--- client/src/components/productEdit/EditProduct.js
+++ client/src/components/productEdit/EditProduct.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import axios from 'axios';
 import React, { Component } from 'react';
-import { reduxForm, Field, initialize } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 import { Redirect } from 'react-router';
 import ProductField from './ProductField';
 import productFormFields from './productFormFields';
+import TextEditor from './TextEditor';
 import { withStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import { Send } from '@material-ui/icons';
@@ -17,7 +18,6 @@ class EditProduct extends Component {
     };
 
     this.id = props.match.params.id;
-    console.log(this.id);
   }
 
   async componentDidMount() {
@@ -26,7 +26,7 @@ class EditProduct extends Component {
     this.props.initialize({ category, name, details });
   }
 
-  renderFields() {
+  renderBasicFields() {
     return _.map(productFormFields, ({ label, name }) => {
       return (
         <Field
@@ -40,10 +40,14 @@ class EditProduct extends Component {
     });
   }
 
-  async onSubmit(values) {
-    const res = await axios.put(`/api/product/${this.id}`, values);
-    this.id = res.data._id;
+  renderDetailsEditor() {
+    return (
+      <Field key="details" component={TextEditor} type="text" name="details" />
+    );
+  }
 
+  async onSubmit(values) {
+    await axios.put(`/api/product/${this.id}`, values);
     this.setState({ isDone: true });
   }
 
@@ -54,7 +58,8 @@ class EditProduct extends Component {
       <div>
         상품 수정
         <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
-          {this.renderFields()}
+          {this.renderBasicFields()}
+          {this.renderDetailsEditor()}
           <Button variant="raised" color="primary" type="submit">
             <Send className={classes.icon} />
             완료
@@ -75,7 +80,12 @@ const styles = theme => ({
 
 function validate(values) {
   const errors = {};
-  // TODO: 여기에 validation 구현
+
+  ['name', 'category', 'details'].forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required';
+    }
+  });
 
   return errors;
 }
