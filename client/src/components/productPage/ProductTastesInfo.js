81c7fe36e24d7df23f723dd9ec9aa71863ca03a1
seoco <seokho.dev@gmail.com>
2018-08-04 09:48:30 +0900

    상품 페이지 평점 분포, 맛 수치 차트 출력 구현

--- client/src/components/productPage/ProductTastesInfo.js
+++ client/src/components/productPage/ProductTastesInfo.js
@@ -1,25 +1,45 @@
 import React, { Component, Fragment } from 'react';
+import {
+  RadarChart,
+  Radar,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis
+} from 'recharts';
+import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import category from '../../assets/datas/productCategoryDict';
 
 class ProductTastesInfo extends Component {
-  renderTastes() {
-    const { product } = this.props;
-    const taste = category[product.category].params;
-    
-    const tasteList = taste.map((item, i) => {
-      return (
-        <li key={i}>
-          {item}: {product.avgTaste[i]}
-        </li>
-      );
-    });
+  processData(tasteArray) {
+    const tasteNames = category[this.props.product.category].params;
+
+    return tasteArray.map((item, i) => ({ name: tasteNames[i], value: item }));
+  }
+
+  renderChart() {
+    const { avgTaste } = this.props.product;
+    const data = this.processData(avgTaste);
+    console.log(data);
 
     return (
-      <Fragment>
-        <h4>맛 수치 정보</h4>
-        <ul>{tasteList}</ul>
-      </Fragment>
+      <RadarChart outerRadius={80} width={240} height={240} data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="name" tick={{ fontSize: 12 }} />
+        <PolarRadiusAxis
+          type="number"
+          domain={[0, 5]}
+          angle={15}
+          axisLine={false}
+          scale="linear"
+        />
+        <Radar
+          dataKey="value"
+          stroke="#ffa826"
+          fill="#ffc126"
+          fillOpacity={0.4}
+        />
+      </RadarChart>
     );
   }
 
@@ -30,15 +50,17 @@ class ProductTastesInfo extends Component {
       return <div />;
     }
 
-    return <Fragment>{this.renderTastes()}</Fragment>;
+    return (
+      <div>
+        <Typography variant="subheading">맛 수치</Typography>
+        {this.renderChart()}
+      </div>
+    );
   }
 }
 
 const styles = theme => ({
-  icon: {
-    marginRight: theme.spacing.unit,
-    fontSize: 20
-  }
+  //
 });
 
 export default withStyles(styles)(ProductTastesInfo);
