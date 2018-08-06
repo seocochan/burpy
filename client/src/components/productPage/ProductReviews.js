c9e89814df8028edaeee66ca410d0dc2601bd3eb
seoco <seokho.dev@gmail.com>
2018-08-06 23:56:13 +0900

    리스트 출력 형태 변경

--- client/src/components/productPage/ProductReviews.js
+++ client/src/components/productPage/ProductReviews.js
@@ -1,6 +1,17 @@
 import _ from 'lodash';
-import axios from 'axios';
 import React, { Component, Fragment } from 'react';
+import Rating from 'react-rating';
+import { ThumbUp, Star, StarBorder } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  Paper,
+  Typography,
+  IconButton
+} from '@material-ui/core';
 
 class ProductReviews extends Component {
   constructor(props) {
@@ -11,37 +22,95 @@ class ProductReviews extends Component {
   }
 
   renderReviews() {
-    const { reviews } = this.props;
+    const { reviews, classes } = this.props;
 
     if (this.hasReview) {
       return _.map(reviews, item => {
+        const Content = () => (
+          <Paper classname={classes.content} elevation={0}>
+            <Typography variant="subheading">
+              {item.userId.name}
+              <Rating
+                readonly
+                initialRating={parseInt(item.score)}
+                fullSymbol={
+                  <Star className={classes.starIcon} nativeColor="#ffda00" />
+                }
+                emptySymbol={
+                  <StarBorder
+                    className={classes.starIcon}
+                    nativeColor="#ffed87"
+                  />
+                }
+              />
+            </Typography>
+            <Typography variant="body1" component="p">
+              {item.comment}
+            </Typography>
+          </Paper>
+        );
+
+        const Footer = () => (
+          <Typography className={classes.footer} variant="caption">
+            {item.dateAdded}
+          </Typography>
+        );
+
         return (
-          <li key={item._id}>
-            ID: {item.userId.name}
-            평점: {item.score}
-            코멘트: {item.comment}
-          </li>
+          <ListItem key={item._id} divider disableGutters>
+            <ListItemText
+              disableTypography
+              primary={<Content />}
+              secondary={<Footer />}
+            />
+            <ListItemSecondaryAction>
+              <IconButton>
+                <ThumbUp style={{ fontSize: 18 }} />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
         );
       });
     } else {
       return (
-        <div>
-          <p>아직 사용자들의 리뷰가 없습니다.</p>
-        </div>
+        <Typography variant="subheading">
+          아직 사용자들의 리뷰가 없습니다.
+        </Typography>
       );
     }
   }
 
   render() {
+    const { classes } = this.props;
     this.hasReview = this.props.reviews.length != 0 ? true : false;
 
     return (
-      <Fragment>
-        <h4>리뷰 목록</h4>
-        <ul>{this.renderReviews()}</ul>
-      </Fragment>
+      <div className={classes.container}>
+        <List className={classes.list} dense>
+          {this.renderReviews()}
+        </List>
+      </div>
     );
   }
 }
 
-export default ProductReviews;
+const styles = theme => ({
+  container: {
+    width: '100%'
+  },
+  list: {
+    margin: theme.spacing.unit
+  },
+  content: {
+    minHeight: 48
+  },
+  footer: {
+    marginTop: theme.spacing.unit * 2
+  },
+  starIcon: {
+    fontSize: 16,
+    marginBottom: '-2px'
+  }
+});
+
+export default withStyles(styles)(ProductReviews);
