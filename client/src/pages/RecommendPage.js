dffeda061090c7ef1cdabb96919ae35df6c8c201
seoco <seokho.dev@gmail.com>
2018-08-29 20:56:52 +0900

    Recommend 페이지 레이아웃 수정

--- client/src/pages/RecommendPage.js
+++ client/src/pages/RecommendPage.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import category from '../assets/datas/productCategoryDict';
+import ProductCardVertical from '../components/ProductCardVertical';
 
 class RecommendPage extends Component {
   constructor(props) {
@@ -20,31 +21,44 @@ class RecommendPage extends Component {
   }
 
   renderList() {
+    const { classes } = this.props;
     const { recommended } = this.state;
 
     return _.map(category, c => {
       if (recommended.hasOwnProperty(c.eng) && recommended[c.eng]) {
-        let itemList = [];
+        const items = recommended[c.eng].map(item => (
+          <ProductCardVertical key={item.id} product={item} />
+        ));
 
-        recommended[c.eng].forEach(item => {
-          itemList.push(
-            <li key={item.id}>
-              <Link to={`/product/${item.id}`}>{item.name}</Link>
-              {` / 예상 평점: ${item.score}`}
-            </li>);
-        });
-
+        // let itemList = [];
+        // recommended[c.eng].forEach(item => {
+        //   itemList.push(
+        //     <li key={item.id}>
+        //       <Link to={`/product/${item.id}`}>{item.name}</Link>
+        //       {` / 예상 평점: ${item.score}`}
+        //     </li>
+        //   );
+        // });
+        //
         return (
-          <div key={c.eng}>
-            <h2>{c.kor}</h2>
-            <ul>{itemList}</ul>
+          <div className={classes.productSection} key={c.eng}>
+            <Typography variant="headline" gutterBottom>
+              {c.kor}
+            </Typography>
+            <div className={classes.listContainer}>
+              <div className={classes.list}>{items}</div>
+            </div>
           </div>
         );
       } else {
         return (
-          <div key={c.eng}>
-            <h2>{c.kor}</h2>
-            <ul>아직 추천 상품이 없습니다. 더 많은 리뷰를 등록하세요.</ul>
+          <div className={classes.productSection} key={c.eng}>
+            <Typography variant="headline" gutterBottom>
+              {c.kor}
+            </Typography>
+            <div className={classes.listContainer}>
+              아직 추천 상품이 없습니다. 더 많은 리뷰를 등록하세요.
+            </div>
           </div>
         );
       }
@@ -52,15 +66,50 @@ class RecommendPage extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { recommended } = this.state;
 
     return (
-      <div>
-        <h2>추천 상품 목록</h2>
+      <div className={classes.container}>
+        <Typography variant="headline" gutterBottom>
+          추천 상품 목록
+        </Typography>
         {recommended && this.renderList()}
       </div>
     );
   }
 }
 
-export default RecommendPage;
+const styles = theme => ({
+  container: {
+    width: '100%',
+    maxWidth: 1280,
+    margin: 'auto'
+  },
+  productSection: {
+    margin: 'auto',
+    marginBottom: theme.spacing.unit * 3,
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: (120 + theme.spacing.unit / 2) * 5 + 20
+    },
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: (140 + theme.spacing.unit / 2) * 5 + 20
+    },
+    [theme.breakpoints.up('md')]: {
+      maxWidth: (180 + theme.spacing.unit) * 5 + 40
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: (200 + theme.spacing.unit) * 5 + 40
+    }
+  },
+  listContainer: {
+    overflowX: 'auto'
+  },
+  list: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    width: 0
+  }
+});
+
+export default withStyles(styles)(RecommendPage);
