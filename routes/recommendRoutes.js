dde36b1e385aa63b7f91a16ae1ed0face2b88a48
seoco <seokho.dev@gmail.com>
2018-09-05 23:49:24 +0900

    로그인 요구 미들웨어 적용

--- routes/recommendRoutes.js
+++ routes/recommendRoutes.js
@@ -1,9 +1,10 @@
 const RecommendControllers = require('../controllers/RecommendControllers');
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.get('/api/train/:id', RecommendControllers.trainRequest);
-  app.get('/api/train', RecommendControllers.trainRequest);
-  app.get('/api/predict/:id', RecommendControllers.predictRequest);
-  app.get('/api/predict', RecommendControllers.predictRequest);
-  app.get('/api/recommend', RecommendControllers.fetchRecommendItems);
+  app.get('/api/train/:id', requireLogin, RecommendControllers.trainRequest);
+  app.get('/api/train', requireLogin, RecommendControllers.trainRequest);
+  app.get('/api/predict/:id', requireLogin, RecommendControllers.predictRequest);
+  app.get('/api/predict', requireLogin, RecommendControllers.predictRequest);
+  app.get('/api/recommend', requireLogin, RecommendControllers.fetchRecommendItems);
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const UserControllers = require('../controllers/UserControllers');
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.get('/api/wishlist', UserControllers.fetchWishlist);
-  app.delete('/api/wishlist/:id', UserControllers.deleteWishlistItem);
-  app.get('/api/my-products', UserControllers.fetchMyProduct);
-  app.post('/api/wishlist/:id',UserControllers.addWishlist);
-  app.get('/api/myinfo',UserControllers.fetchMyInfo);
-  app.put('/api/myinfo/:id',UserControllers.updateMyInfo);
+  app.get('/api/wishlist', requireLogin, UserControllers.fetchWishlist);
+  app.delete('/api/wishlist/:id', requireLogin, UserControllers.deleteWishlistItem);
+  app.get('/api/my-products', requireLogin, UserControllers.fetchMyProduct);
+  app.post('/api/wishlist/:id', requireLogin, UserControllers.addWishlist);
+  app.get('/api/myinfo', requireLogin, UserControllers.fetchMyInfo);
+  app.put('/api/myinfo/:id', requireLogin, UserControllers.updateMyInfo);
 };
