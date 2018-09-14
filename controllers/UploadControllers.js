693f5c632e9810187cdd097f21c569badcc8c5bf
seoco <seokho.dev@gmail.com>
2018-09-15 01:29:10 +0900

    이미지 업로드시 포인트 지급 구현

--- controllers/UploadControllers.js
+++ controllers/UploadControllers.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const User = require('../models/User');
 const uuid = require('uuid/v1');
 const keys = require('../config/keys');
 
@@ -10,16 +11,27 @@ const s3 = new AWS.S3({
 
 module.exports = {
   getImageUrl(req, res) {
-    const { query } = req;
+    const { query, user } = req;
 
     if (Object.keys(query).length === 0) {
       return res.status(400).send({ error: '잘못된 요청입니다.' });
     }
 
     const { category, name } = query;
-    const { key = `${category}/${name}/${uuid()}.jpeg` } = query;
-    // key를 전달받으면 기존 이미지를 수정하는 경우, 아니면 새로 등록하는 경우임.
-    // 값이 없는 경우 uuid로 새로운 key 값을 지정함.
+    let { key } = query;
+
+    // 이미지가 없던 상품에 새로 등록되는 경우
+    if (!key) {
+      User.findByIdAndUpdate(user.id, { $inc: { points: 3 } }).exec(
+        (err, doc) => {
+          if (err) {
+            return res.status(500).send({ error: 'DB 에러: ' + err });
+          }
+        }
+      );
+
+      key = `${category}/${name}/${uuid()}.jpeg`;
+    }
 
     s3.getSignedUrl(
       'putObject',
