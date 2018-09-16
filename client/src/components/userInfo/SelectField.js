29df32905760f4b4984519cd239c0cfa55bf48e4
LeeYoonSang <kty189@naver.com>
2018-09-16 23:41:55 +0900

    내정보수정페이지 수정

--- client/src/components/userInfo/SelectField.js
+++ client/src/components/userInfo/SelectField.js
@@ -12,11 +12,11 @@ export default ({ input, label, meta: { touched, error } }) => {
     <FormControl component="fieldset">
       <FormLabel component="legend">{label}</FormLabel>
       <RadioGroup aria-label="Gender" name="gender" {...input}>
-        <FormControlLabel value="남자" control={<Radio />} label="남자" />
-        <FormControlLabel value="여자" control={<Radio />} label="여자" />
+        <FormControlLabel value="남자" control={<Radio color='primary' />} label="남자" />
+        <FormControlLabel value="여자" control={<Radio color='primary' />} label="여자" />
         <FormControlLabel
           value="고르고 싶지 않다"
-          control={<Radio />}
+          control={<Radio color='primary' />}
           label="고르고 싶지 않다"
         />
       </RadioGroup>
