cddc1d9e97734b62752ebf2582d7ebd32b368a42
seoco <seokho.dev@gmail.com>
2018-09-23 18:52:31 +0900

    뱃지 아이콘 추가

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,6 @@
+export { default as LogoIcon } from './LogoIcon';
+export { default as BeerIcon } from './BeerIcon';
+export { default as PhotoIcon } from './PhotoIcon';
+export { default as SodaIcon } from './SodaIcon';
+export { default as CoffeeIcon } from './CoffeeIcon';
+export { default as WhiskeyIcon } from './WhiskeyIcon';
